af274aee22697892f2ef129157dcc7005c422ab8
jjlr <mc.jjlr@tutanota.com>
2019-08-14 04:38:51 -0700

    Initial commit of new project.

---
+++ src/main/java/jjlr/relicspast/RelicsPastMain.java
@@ -0,0 +1,14 @@
+package jjlr.relicspast;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("relicsofthepast")
+public class RelicsPastMain {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	
+}
